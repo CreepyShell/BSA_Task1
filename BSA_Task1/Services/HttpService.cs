4740528abe627dcdc51666b96364080ed7e468ea
Danil <danil.t404@gmail.com>
2021-06-22 22:27:15 +0300

    Ready project in one commit)

---
+++ BSA_Task1/Services/HttpService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+
+namespace BSA_Task1.Services
+{
+    public class HttpService
+    {
+        private HttpClient client = new HttpClient();
+
+        public HttpResponseMessage GetProjects() =>
+            client.GetAsync("https://bsa21.azurewebsites.net/api/Projects")
+            .Result;
+
+        public HttpResponseMessage GetTasks() =>
+            client.GetAsync("https://bsa21.azurewebsites.net/api/Tasks")
+            .Result;
+
+        public HttpResponseMessage GetTeams() =>
+            client.GetAsync("https://bsa21.azurewebsites.net/api/Teams")
+            .Result;
+
+        public HttpResponseMessage GetUsers() =>
+            client.GetAsync("https://bsa21.azurewebsites.net/api/Users")
+            .Result;
+    }
+}
